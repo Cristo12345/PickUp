938ab30707738d7aec3df00fd2918a48b7f4e050
Idil <idilmengulluoglu@gmail.com>
2019-08-06 14:42:17 -0400

    fixed issue where npm start would just run the server.js file again

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -25,6 +25,6 @@ app.use(express.static("public"));
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-    
   });
 });
+
