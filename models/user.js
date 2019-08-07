93890a34e743ab069ec8f891b43a596a4fd2d110
Roshini Jaiswal <roshinijaiswal@dyn-160-39-11-59.dyn.columbia.edu>
2019-08-07 10:34:32 -0400

    Merged updates from group

--- models/user.js
+++ models/user.js
@@ -1,12 +1,22 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
-  User.associate = function(models) {
-    // associations can be defined here
-  };
-  return User;
-};
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+module.exports = User = mongoose.model("users", UserSchema);
